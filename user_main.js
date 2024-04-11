86ae4a71b5941081728ed656ede57965f1fac086
sagnik <rsagnik58@gmail.com>
2024-04-11 23:15:50 +0530

    commit

---
+++ user_main.js
@@ -0,0 +1,59 @@
+document.querySelectorAll(".dropdown-toggle").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    if (event.target.classList.contains("dropdown-toggle")) {
+      event.target.classList.toggle("toggle-change");
+    } else if (
+      event.target.parentElement.classList.contains("dropdown-toggle")
+    ) {
+      event.target.parentElement.classList.toggle("toggle-change");
+    }
+  });
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+  .querySelector(".prepend-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide([
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+    ]);
+  });
+document
+  .querySelector(".prepend-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide(
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+    );
+  });
+document.querySelector(".append-slide").addEventListener("click", function (e) {
+  e.preventDefault();
+  swiper.appendSlide(
+    '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+  );
+});
+document
+  .querySelector(".append-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide([
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+    ]);
+  });
