ebb66d04c953fc73773de59f4d86bebba0275bb9
sayan <sayankarmakar09990@gmail.com>
2024-05-18 15:31:54 +0530

    revert dockerize for kafka,zookeeper disconnection

--- SpringBoot-Backend/springboot/src/main/java/com/smartcare/SmartCare/Kafka/Config/AppConstants.java
+++ SpringBoot-Backend/springboot/src/main/java/com/smartcare/SmartCare/Kafka/Config/AppConstants.java
@@ -2,5 +2,5 @@ package com.smartcare.SmartCare.Kafka.Config;
 
 public class AppConstants {
     public static final String RequestTopicName = "RequestToNgo";
-    public static final String  GROUP_ID="group-1";
+    public static final String  GROUP_ID="group-4";
 }
