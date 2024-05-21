4c1d328ea6ca27b1cb29d733069428b91bf0a24a
sayan <sayankarmakar09990@gmail.com>
2024-05-21 23:05:28 +0530

    test

--- SpringBoot-Backend/springboot/src/main/java/com/smartcare/SmartCare/Config/CorsConfig.java
+++ SpringBoot-Backend/springboot/src/main/java/com/smartcare/SmartCare/Config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc //adding this 
 public class CorsConfig implements WebMvcConfigurer{
     @Bean
     public WebMvcConfigurer corsConfigurer() {
