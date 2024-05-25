00848d14e3b2a4738feb362ff6d74c86312fbc07
sayan <sayankarmakar09990@gmail.com>
2024-05-25 15:45:50 +0530

    delete owner dto duplicate variable

--- SpringBoot-Backend/springboot/src/main/java/com/smartcare/SmartCare/DTO/OwnerDTO.java
+++ SpringBoot-Backend/springboot/src/main/java/com/smartcare/SmartCare/DTO/OwnerDTO.java
@@ -1,7 +1,6 @@
 package com.smartcare.SmartCare.DTO;
 
 
-//import jakarta.annotation.sql.DataSourceDefinitions;
 import lombok.*;
 
 @Setter
@@ -19,12 +18,4 @@ public class OwnerDTO {
     private String ngoId;
     private String ngoAddress;
     private String password;
-<<<<<<< HEAD
-    private String longitude;
-    private String latitude;
-=======
-    
-    
-   
->>>>>>> cbb02ed6f12e7e37713c6d11b75f22163d267311
 }
